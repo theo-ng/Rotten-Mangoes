430d5cbd73e8ba9f0b34d9cc17865001ca9e2f88
Theo <theo-ng@users.noreply.github.com>
2015-11-23 15:11:42 -0800

    day 1 finished w/o extra credit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
+end
