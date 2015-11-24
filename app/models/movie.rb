efc066a2f3bad4e88b839ba499f3abe87a84600e
Theo <theo-ng@users.noreply.github.com>
2015-11-24 11:04:28 -0800

    bug fixes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,7 +22,7 @@ class Movie < ActiveRecord::Base
   validate :release_date_is_in_the_past
 
   def review_average
-    reviews.sum(:rating_out_of_ten)/reviews.size
+    reviews.sum(:rating_out_of_ten)/reviews.size if reviews.size > 0
   end
 
   protected
--- app/models/review.rb
+++ app/models/review.rb
@@ -15,7 +15,7 @@ class Review < ActiveRecord::Base
     numericality: { only_integer: true }
   validates :rating_out_of_ten,
     numericality: { greater_than_or_equal_to: 1 }
-  validates :rating_out_of_ten
+  validates :rating_out_of_ten,
     numericality: { less_than_or_equal_to: 10 }
     
 end
