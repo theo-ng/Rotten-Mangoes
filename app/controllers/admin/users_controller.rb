0c1eb5bc39f0e2291d79bb3127106ce0e6be2631
Theo <theo-ng@users.noreply.github.com>
2015-11-24 17:34:55 -0800

    flash msging works now

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,8 +3,8 @@ class Admin::UsersController < ApplicationController
     if current_user && current_user.admin
       @users = User.all
     else
-      # restrict_access
-      redirect_to root_path, notice: "You are not an admin"
+      # flash[:notice] = "You are not an admin"
+      redirect_to root_path, alert: "You are not an admin"
     end
   end
 end
