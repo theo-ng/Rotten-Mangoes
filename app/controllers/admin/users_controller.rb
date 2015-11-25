973b02ba67b24a9afa9fc322b96e73df34976acc
Theo <theo-ng@users.noreply.github.com>
2015-11-24 18:58:10 -0800

    create/edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,4 +7,37 @@ class Admin::UsersController < ApplicationController
       redirect_to root_path, alert: "You are not an admin"
     end
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "#{@user.firstname} #{@user.lastname} created!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path, notice: "#{@user.firstname} #{@user.lastname} updated!"
+    else
+      render :edit
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
 end
